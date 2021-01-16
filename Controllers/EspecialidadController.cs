bb7ca5d25cc47a9f3f32711a06558b893d115dc2
Jhonier Riascos Zapata <jhonierrz@gmail.com>
2021-01-16 15:29:45 -0500

    Método y vista Create

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -79,5 +79,22 @@ namespace Control_Medico_NS.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("PubIntIdEspecialidad,PubStrDescripcion")] Especialidad especialidad)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
     }
 }
